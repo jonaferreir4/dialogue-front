2fbe99f1983f67d681b0eda15eceb7a616134ad8
jonaferreir4 <jonaferreira2204@gmail.com>
2025-11-14 23:42:50 -0300

    feat(app): create chat context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { Chat } from "./pages/chat"
 import { Welcome } from "./pages/welcome"
 import { JoinRoom } from "./pages/join-room"
-
+import { ChatProvider } from "./context/ChatContext"
 function App() {
 
   return (
     <div>
-      <Routes>
+      <ChatProvider>
+        <Routes>
           <Route path="/" element={<Navigate to="/welcome" replace />} />
           <Route path="/join-room" element={<JoinRoom/>}/>
-        <Route path="/chat" element={<Chat/>}/>
-        <Route path="/welcome" element={<Welcome/>}/>
-        <Route path="/join-room" element={<JoinRoom/>}/>
-      </Routes>
+          <Route path="/chat" element={<Chat/>}/>
+          <Route path="/welcome" element={<Welcome/>}/>
+        </Routes>
+      </ChatProvider>
     </div>
   )
 }
