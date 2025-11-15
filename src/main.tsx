ca368c69ef17573747c0a4c15bbaaa9da33a39da
jonaferreir4 <jonaferreira2204@gmail.com>
2025-11-14 23:43:34 -0300

    feat(main): add cssBaseline from material  ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import { CssBaseline } from '@mui/material'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <CssBaseline/>
       <App />
     </BrowserRouter>
   </StrictMode>,
